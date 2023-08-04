5ed916374559a9258e64e83ffba6c587461e9f97
amelakor <amela_korjenic@hotmail.com>
2023-08-04 15:35:40 +0200

    Generating summary route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { SummaryModule } from './summary/summary.module';
 
 @Module({
-  imports: [],
+  imports: [SummaryModule],
   controllers: [AppController],
   providers: [
     AppService,
