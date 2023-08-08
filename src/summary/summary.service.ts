1e10ef399c90e3ec51726dcb0deb1c3adce574e5
amelakor <amela_korjenic@hotmail.com>
2023-08-08 12:46:31 +0200

    Expenses summary

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ReportType } from 'src/data';
+import { ReportService } from 'src/report/report.service';
 
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
+  getSummary() {
+    console.log(this.reportService);
+    const incomes = this.reportService.getAllReports(ReportType.INCOME);
+    const expenses = this.reportService.getAllReports(ReportType.EXPENSE);
+
+    const totalIncome = incomes.reduce((acc, cur) => acc + cur.amount, 0);
+    const totalExpense = expenses.reduce((acc, cur) => acc + cur.amount, 0);
+    return {
+      totalIncome,
+      totalExpense,
+      balance: totalIncome - totalExpense,
+    };
+  }
+}
